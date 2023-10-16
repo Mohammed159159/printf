fbfacd8a87b6c11bdd9567dc4a7a9b884564ff88
Mohammed <mohammedhany300gmail.com>
2023-10-16 22:56:00 +0300

    dev: printf-task_0

---
+++ printf.c
@@ -0,0 +1,66 @@
+#include "main.h"
+
+/**
+ * _printf - an augmentation for printf
+ * @format: formatted string
+ * Return: no. chars of format
+ */
+int _printf(const char *format, ...)
+{
+	unsigned int len = 0, temp_len = 0;
+	char c;
+	char *str;
+	va_list arg_list;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(arg_list, format);
+
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			len++;
+		}
+		else
+		{
+			format++;
+
+			if (*format == '\0')
+				break;
+
+			if (*format == '%')
+			{
+				write(1, format, 1);
+				len++;
+			}
+			if (*format == 'c')
+			{
+				c = va_arg(arg_list, int);
+				write(1, &c, 1);
+				len++;
+			}
+
+			if (*format == 's')
+			{
+				str = va_arg(arg_list, char*);
+
+				while (str[temp_len] != '\0')
+					temp_len++;
+
+				write(1, str, temp_len);
+
+				len += temp_len;
+			}
+		}
+
+		format++;
+	}
+
+	va_end(arg_list);
+
+	return (len);
+}
+
