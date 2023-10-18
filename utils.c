40f90e72f1b42c82d298b3d27602bb081e8b130f
Mohammed <mohammedhany300gmail.com>
2023-10-18 04:02:51 +0300

    fix: Hot fix

--- utils.c
+++ utils.c
@@ -16,6 +16,9 @@ int print_string(char *str)
 		return (6);
 	}
 
+	if (str[0] == '\0')
+		return (0);
+
 	while (str[temp_len] != '\0')
 		temp_len++;
 
@@ -31,6 +34,8 @@ int print_string(char *str)
  */
 int print_char(char ch)
 {
+	if (ch == '\0')
+		return (0);
 
 	write(1, &ch, 1);
 	return (1);
