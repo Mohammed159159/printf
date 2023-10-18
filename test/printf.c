4b311a97d705a2172fb4b0f20f1f1b982f34117c
Mohammed <mohammedhany300gmail.com>
2023-10-18 05:02:14 +0300

    fix: Hot fix

--- test/printf.c
+++ test/printf.c
@@ -1,13 +1,49 @@
 #include "main.h"
 
 /**
+ * print_string - a function for printing a string
+ * @str: string to be printed
+ * Return: length of string printed
+ */
+int print_string(char *str)
+{
+	int temp_len = 0;
+	char *null_string = "(null)";
+
+	if (str == NULL)
+	{
+		write(1, null_string, 6);
+		return (6);
+	}
+
+	while (str[temp_len] != '\0')
+		temp_len++;
+
+	write(1, str, temp_len);
+
+	return (temp_len);
+}
+
+/**
+ * print_char - a function for printing a char
+ * @ch: char to be printed
+ * Return: 1 or 6 if char is null
+ */
+int print_char(char ch)
+{
+
+	write(1, &ch, 1);
+	return (1);
+}
+
+/**
  * _printf - an augmentation for printf
  * @format: formatted string
  * Return: no. chars of format
  */
 int _printf(const char *format, ...)
 {
-	int len = 0, temp_len = 0;
+	int len = 0;
 	char c;
 	char *str;
 	va_list arg_list;
@@ -17,7 +53,7 @@ int _printf(const char *format, ...)
 
 	va_start(arg_list, format);
 
-	while (*format)
+	while (*format && format)
 	{
 		if (*format != '%')
 		{
@@ -29,31 +65,26 @@ int _printf(const char *format, ...)
 			format++;
 
 			if (*format == '\0')
-				break;
+				return (-1);
 
 			if (*format == '%')
 			{
 				write(1, format, 1);
 				len++;
 			}
-			if (*format == 'c')
+			else if (*format == 'c')
 			{
 				c = va_arg(arg_list, int);
-				write(1, &c, 1);
-				len++;
+				len += print_char(c);
 			}
 
-			if (*format == 's')
+			else if (*format == 's')
 			{
 				str = va_arg(arg_list, char*);
-
-				while (str[temp_len] != '\0')
-					temp_len++;
-
-				write(1, str, temp_len);
-
-				len += temp_len;
+				len += print_string(str);
 			}
+			else
+				return (-1);
 		}
 
 		format++;
@@ -63,4 +94,3 @@ int _printf(const char *format, ...)
 
 	return (len);
 }
-
---
+++ utils.c
@@ -0,0 +1,94 @@
+#include "main.h"
+
+/**
+ * print_string - a function for printing a string
+ * @str: string to be printed
+ * Return: length of string printed
+ */
+int print_string(char *str)
+{
+	int temp_len = 0;
+	char *null_string = "(null)";
+
+	if (str == NULL)
+	{
+		write(1, null_string, 6);
+		return (6);
+	}
+
+	while (str[temp_len] != '\0')
+		temp_len++;
+
+	write(1, str, temp_len);
+
+	return (temp_len);
+}
+
+/**
+ * print_char - a function for printing a char
+ * @ch: char to be printed
+ * Return: 1 or 6 if char is null
+ */
+int print_char(char ch)
+{
+	write(1, &ch, 1);
+	return (1);
+}
+
+/**
+ * print_dec - prints a decimal
+ * @n: number to be printed
+ * Return: no. chars printed
+ */
+int print_dec(int n)
+{
+	char ch;
+	int i = 0;
+
+	if (n % 10 < 0)
+	{
+		n = -n;
+		ch = '-';
+		write(1, &ch, 1);
+		i++;
+	}
+
+	if (n / 10 == 0)
+	{
+		ch = n + '0';
+		write(1, &ch, 1);
+		i++;
+		return (i);
+	}
+	else
+	{
+		i += print_dec(n / 10);
+		ch = n % 10 + '0';
+		write(1, &ch, 1);
+		return (++i);
+	}
+}
+
+/**
+ * print_bin - prints unsigned int in binary format
+ * @n: number to be printed
+ * Return: number of chars printed
+ */
+int print_bin(unsigned int n)
+{
+	int len = 0;
+	char ch;
+
+	if (n == 1)
+	{
+		ch = 1 + '0';
+		write(1, &ch, 1);
+		len++;
+		return (len);
+	}
+	
+	len += print_bin(n / 2);
+	ch = (n % 2) + '0';
+	write(1, &ch, 1);
+	return (++len);
+}
