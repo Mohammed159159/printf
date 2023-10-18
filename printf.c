2a88150e3182a9e9676ce24630de73da65f20156
Mohammed <mohammedhany300gmail.com>
2023-10-18 04:40:35 +0300

    fix: Hot fix

--- printf.c
+++ printf.c
@@ -1,49 +1,5 @@
 #include "main.h"
-
-/**
- * print_string - a function for printing a string
- * @s: string to be printed
- * Return: length of string printed
- */
-int print_string(char *str)
-{
-	int temp_len = 0;
-	char *null_string = "(null)";
-
-	if (str == NULL)
-	{
-		write(1, null_string, 6);
-		return (6);
-	}
-
-	while (str[temp_len] != '\0')
-		temp_len++;
-
-	write(1, str, temp_len);
-
-	return (temp_len);
-}
-
-/**
- * print_char - a function for printing a char
- * @s: char to be printed
- * Return: 1 or 6 if char is null
- */
-int print_char(char ch)
-{
-	char *null_string = "(null)";
-
-	if (ch == NULL)
-	{
-		write(1, null_string, 6);
-		return (6);
-	}
-
-	write(1, &ch, 1);
-
-	return (1);
-}
-
+#include <stdio.h>
 /**
  * _printf - an augmentation for printf
  * @format: formatted string
@@ -51,53 +7,64 @@ int print_char(char ch)
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	char c;
-	char *str;
+	char c, *str;
+	int len = 0, n;
 	va_list arg_list;
 
-	if (format == NULL ||
-		(format[0] == '%' && format[1] == '\0'))
+	if (format == NULL)
 		return (-1);
+
+	if (*format == '\0')
+		return (0);
 
 	va_start(arg_list, format);
 
-	while (*format)
+	while (*format && format)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			len++;
+			write(1, format, 1); len++;
 		}
 		else
 		{
 			format++;
-
 			if (*format == '\0')
-				break;
-
+				return (-1);
 			if (*format == '%')
 			{
 				write(1, format, 1);
 				len++;
 			}
-			if (*format == 'c')
+			else if (*format == 'c')
 			{
 				c = va_arg(arg_list, int);
 				len += print_char(c);
 			}
-
-			if (*format == 's')
+			else if (*format == 's')
 			{
 				str = va_arg(arg_list, char*);
 				len += print_string(str);
 			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				n = va_arg(arg_list, int);
+				len += print_dec(n);
+			}
+			else if (*format == 'b')
+			{
+				n = va_arg(arg_list, int);
+				len += print_bin(n);
+			}
+			else
+				return (-1);
 		}
-
 		format++;
 	}
-
 	va_end(arg_list);
-
 	return (len);
+}
+
+void main(void)
+{
+	printf("%d");
 }
