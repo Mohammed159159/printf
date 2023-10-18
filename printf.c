4adf6670b405312429da4c7670a13f961b88d511
Mohammed <mohammedhany300gmail.com>
2023-10-18 03:53:22 +0300

    dev: printf-b

--- printf.c
+++ printf.c
@@ -1,86 +1,14 @@
 #include "main.h"
 
 /**
- * print_string - a function for printing a string
- * @str: string to be printed
- * Return: length of string printed
- */
-int print_string(char *str)
-{
-	int temp_len = 0;
-	char *null_string = "(null)";
-
-	if (str == NULL)
-	{
-		write(1, null_string, 6);
-		return (6);
-	}
-
-	while (str[temp_len] != '\0')
-		temp_len++;
-
-	write(1, str, temp_len);
-
-	return (temp_len);
-}
-
-/**
- * print_char - a function for printing a char
- * @ch: char to be printed
- * Return: 1 or 6 if char is null
- */
-int print_char(char ch)
-{
-
-	write(1, &ch, 1);
-	return (1);
-}
-
-/**
- * print_dec - prints a decimal
- * @n: number to be printed
- * Return: no. chars printed
- */
-int print_dec(int n)
-{
-	char ch;
-	int i = 0;
-
-	if (n % 10 < 0)
-	{
-		n = -n;
-		ch = '-';
-		write(1, &ch, 1);
-		i++;
-	}
-
-	if (n / 10 == 0)
-	{
-		ch = n + '0';
-		write(1, &ch, 1);
-		i++;
-		return (i);
-	}
-	else
-	{
-		i += print_dec(n / 10);
-		ch = n % 10 + '0';
-		write(1, &ch, 1);
-		return (++i);
-	}
-}
-
-/**
  * _printf - an augmentation for printf
  * @format: formatted string
  * Return: no. chars of format
  */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	char c;
-	char *str;
-	int n;
+	char c, *str;
+	int len = 0, n;
 	va_list arg_list;
 
 	if (format == NULL)
@@ -92,16 +20,13 @@ int _printf(const char *format, ...)
 	{
 		if (*format != '%')
 		{
-			write(1, format, 1);
-			len++;
+			write(1, format, 1); len++;
 		}
 		else
 		{
 			format++;
-
 			if (*format == '\0')
 				return (-1);
-
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -112,7 +37,6 @@ int _printf(const char *format, ...)
 				c = va_arg(arg_list, int);
 				len += print_char(c);
 			}
-
 			else if (*format == 's')
 			{
 				str = va_arg(arg_list, char*);
@@ -123,14 +47,16 @@ int _printf(const char *format, ...)
 				n = va_arg(arg_list, int);
 				len += print_dec(n);
 			}
+			else if (*format == 'b')
+			{
+				n = va_arg(arg_list, int);
+				len += print_bin(n);
+			}
 			else
 				return (-1);
 		}
-
 		format++;
 	}
-
 	va_end(arg_list);
-
 	return (len);
 }
